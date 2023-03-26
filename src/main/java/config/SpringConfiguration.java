7f315cccddf4a9fbe4f46ee0bf8baf08f75515ab
DeadlyMouse223 <blabla.a228@gmail.com>
2023-03-26 18:17:16 +0300

    Initial commit

---
+++ src/main/java/config/SpringConfiguration.java
@@ -0,0 +1,14 @@
+package config;
+
+import model.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+
+    @Bean
+    public Student student(){
+        return new Student();
+    }
+}
